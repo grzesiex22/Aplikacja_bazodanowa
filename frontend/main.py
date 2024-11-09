dfc725feb52064ffc521b3b47bf51e62438568d3
grzesiex22 <grzesiug2002@o2.pl>
2024-11-09 18:00:45 +0100

    Zmiana struktury frontend

---
+++ frontend/main.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5 import QtWidgets
+from ui.main_window import MainWindow
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()  # Tworzymy instancję MainWindow
+    window.show()  # Pokazujemy główne okno
+    sys.exit(app.exec_())
