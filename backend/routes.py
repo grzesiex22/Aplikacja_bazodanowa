8c0080322a463e26163814827c493138d7c524c0
compton22 <272554@student.pwr.edu.pl>
2024-12-02 23:41:10 +0100

    poprawiony magazyn

--- backend/routes.py
+++ backend/routes.py
@@ -7,7 +7,6 @@ from Aplikacja_bazodanowa.backend.routes_dir.typSerwisu_routes import typserwis_
 from Aplikacja_bazodanowa.backend.routes_dir.czesc_routes import czesc_bp
 from Aplikacja_bazodanowa.backend.routes_dir.serwis_routes2 import serwis_bp
 from Aplikacja_bazodanowa.backend.routes_dir.app_routes import app_bp
-from Aplikacja_bazodanowa.backend.routes_dir.wyposazenie_routes import wyposazenie_bp
 
 bp = Blueprint('api', __name__)
 
@@ -16,7 +15,6 @@ bp.register_blueprint(kierowca_bp)
 bp.register_blueprint(pojazd_bp)
 # bp.register_blueprint(pojazd_bp_patryk)
 bp.register_blueprint(typserwis_bp)
-bp.register_blueprint(wyposazenie_bp)
 bp.register_blueprint(czesc_bp)
 bp.register_blueprint(serwis_bp)
 bp.register_blueprint(app_bp)
